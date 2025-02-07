2b74fd2f342e029a03ffb2800a9f9643befca35c
Thomas Greening <tomfgreening@gmail.com>
2025-02-07 10:11:59 +0000

    Imported first photo to use. Caused site to break.

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import rows from "@/components/rows";
 export default function HomePage() {
   return (
     <>
-    <h1>Juicy Jakes Homepage </h1>
-    <h3>Passion for plant-based</h3>
-    {/* /rows  */}
+      <h1>Juicy Jakes Homepage </h1>
+      <h3>Passion for plant-based</h3>
+      <rows />
     </>
-  )
+  );
 }
--- src/components/rows.jsx
+++ src/components/rows.jsx
@@ -1,10 +1,19 @@
-import Link from "next/link";
 import rowStyles from "@components/rows.module.css";
+import Image from "next/image";
+import photo2 from "@../public/assets/images/photo 2.jpg";
 
-export default rows() {
+
+export default function rows() {
     return(
-        <>
-        
-        </>
-    )
+        <photorows className={rowStyles.rows}>
+        <Image
+        src={photo2}
+        alt="Two people riding in a vintage pink car in Las Vegas Nevada."
+        width={500}
+        height={500}
+        priority="false"
+        placeholder="blur"
+        />
+        <photorows/>
+    );
 }
