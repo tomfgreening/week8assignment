d8bf594c4bed95531a4ba4646b629149d0fb8662
Thomas Greening <tomfgreening@gmail.com>
2025-02-07 10:26:55 +0000

    Debugging rows.jsx component.

--- src/components/rows.jsx
+++ src/components/rows.jsx
@@ -1,19 +1,18 @@
-import rowStyles from "@components/rows.module.css";
+import rowStyles from "@/components/rows.module.css";
 import Image from "next/image";
-import photo2 from "@../public/assets/images/photo 2.jpg";
-
+import photo2 from "@/../public/Assets/Images/photo2.jpg";
 
 export default function rows() {
-    return(
-        <photorows className={rowStyles.rows}>
-        <Image
+  return (
+    <container className={rowStyles.rows}>
+      <Image
         src={photo2}
         alt="Two people riding in a vintage pink car in Las Vegas Nevada."
         width={500}
         height={500}
         priority="false"
         placeholder="blur"
-        />
-        <photorows/>
-    );
+      />
+    </container>
+  );
 }
