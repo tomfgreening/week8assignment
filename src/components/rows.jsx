c0d1b8a7f0373f7992806d841fad885099fe8975
Thomas Greening <tomfgreening@gmail.com>
2025-02-07 13:52:36 +0000

    Finsished basic styling for photorows on homepage.

--- src/components/rows.jsx
+++ src/components/rows.jsx
@@ -5,6 +5,11 @@ import photo1 from "@/../public/Assets/Images/photo1.jpg";
 import photo3 from "@/../public/Assets/Images/photo3.jpg";
 import photo4 from "@/../public/Assets/Images/photo4.jpg";
 import photo5 from "@/../public/Assets/Images/photo5.jpg";
+import photo6 from "@/../public/Assets/Images/photo6.jpg";
+import photo7 from "@/../public/Assets/Images/photo7.jpg";
+import photo8 from "@/../public/Assets/Images/photo8.jpg";
+import photo9 from "@/../public/Assets/Images/photo9.jpg";
+import photo10 from "@/../public/Assets/Images/photo10.jpg";
 import acidavian from "@/../public/Assets/Images/acidavian.jpg";
 
 export default function Rows() {
@@ -18,14 +23,14 @@ export default function Rows() {
         priority="false"
         placeholder="blur"
       />
-      <Image
+      {/* <Image
         src={photo1}
         alt="Photograph of retro TVs and gaming consoles."
         width={500}
         height={750}
         priority="false"
         placeholder="blur"
-      />
+      /> */}
       <Image
         src={photo3}
         alt="Photograph of various entertainment machines in an arcade."
@@ -50,7 +55,11 @@ export default function Rows() {
         priority="false"
         placeholder="blur"
       />
-      <Image src={acidavian} height={500} width={500} />
+      <Image src={photo6} alt="Photograph of a commodore 64 gaming system." height={500} width={500} priority="false" placeholder="blur" />
+      <Image src={photo7} alt="Photograph of sunset over Vernazza, Italy."height={500} width={500} priority="false" placeholder="blur"/>
+      <Image src={photo8} alt="Photograph of lava dripping into the ocean from a volcano in Hawaii" height={500} width={500} priority="false" placeholder="blur"/>
+      <Image src={photo9} alt="Photograph of Shibuya crossing in Tokyo, Japan" height={500} width={500} priority="false" placeholder="blur"/>
+      <Image src={photo10} alt="Photograph of dotonbori street in Osaka, Japan"height={500} width={500} priority="false" placeholder="blur"/>
     </container>
   );
 }
