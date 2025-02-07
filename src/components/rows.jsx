1f129ed1ecdf8b2b97cffd017e2a263b82b38cea
Thomas Greening <tomfgreening@gmail.com>
2025-02-07 12:05:07 +0000

    Experimenting with flex to display photos.

--- src/components/rows.jsx
+++ src/components/rows.jsx
@@ -5,6 +5,7 @@ import photo1 from "@/../public/Assets/Images/photo1.jpg";
 import photo3 from "@/../public/Assets/Images/photo3.jpg";
 import photo4 from "@/../public/Assets/Images/photo4.jpg";
 import photo5 from "@/../public/Assets/Images/photo5.jpg";
+import acidavian from "@/../public/Assets/Images/acidavian.jpg";
 
 export default function Rows() {
   return (
@@ -17,38 +18,39 @@ export default function Rows() {
         priority="false"
         placeholder="blur"
       />
-      <Image 
-      src={photo1}
-      alt="Photograph of retro TVs and gaming consoles."
-      width={500}
-      height={750}
-      priority="false"
-      placeholder="blur"
+      <Image
+        src={photo1}
+        alt="Photograph of retro TVs and gaming consoles."
+        width={500}
+        height={750}
+        priority="false"
+        placeholder="blur"
       />
       <Image
-      src={photo3}
-      alt="Photograph of various entertainment machines in an arcade."
-      height={500}
-      width={500}
-      priority="false"
-      placeholder="blur"
+        src={photo3}
+        alt="Photograph of various entertainment machines in an arcade."
+        height={500}
+        width={500}
+        priority="false"
+        placeholder="blur"
       />
       <Image
-       src={photo4}
-       alt="Photograph of a starry milkyway in the sky."
-       height={500}
-       width={500}
-       priority="false"
-       placeholder="blur"
+        src={photo4}
+        alt="Photograph of a starry milkyway in the sky."
+        height={500}
+        width={500}
+        priority="false"
+        placeholder="blur"
       />
       <Image
-      src={photo5}
-      alt="Photograph of an indoor light exhibiton in an art gallery."
-      height={500}
-      width={500}
-      priority="false"
-      placeholder="blur"
+        src={photo5}
+        alt="Photograph of an indoor light exhibiton in an art gallery."
+        height={500}
+        width={500}
+        priority="false"
+        placeholder="blur"
       />
+      <Image src={acidavian} height={500} width={500} />
     </container>
   );
 }
